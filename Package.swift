0437fa3c345d87a00cda3d64ba5e2a4a84915d21
Denis Murphy <dmurphy@dairymaster.com>
2020-03-24 11:12:54 +0000

    Added Swift Package Manager

--- Package.swift
+++ Package.swift
@@ -1,21 +1,19 @@
 // swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "Spring",
-    platforms: [
-          .iOS(.v8),
-          .tvOS(.v9),
-          .macOS(.v10_11),
-    ],
     products: [
         .library(
             name: "Spring",
             targets: ["Spring"]),
     ],
+    dependencies: [],
     targets: [
-        .target(name: "Spring")
-    ],
-    swiftLanguageVersions: [.v5]
+        .target(
+            name: "Spring",
+            dependencies: [])
+    ]
 )
